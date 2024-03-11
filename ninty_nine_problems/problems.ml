2186b9ca82ab5cf59ec72d54ab4e51606e9f272d
Timo Netzer <ecsodikas@gmail.com>
2024-03-11 21:44:36 +0100

    zwico

--- ninty_nine_problems/problems.ml
+++ ninty_nine_problems/problems.ml
@@ -15,6 +15,34 @@ let rec last_two = function
 
 (* 3. Find the K'th element of a list. (easy) *)
 
-let rec kth l k =
-  match l with
-  |
+let rec kth k = function
+  | []               -> None
+  | x::_ when k == 0 -> Some x
+  | _::xs            -> kth (k - 1) xs
+
+(* 4. Find the number of elements of a list. (easy) *)
+
+let count l =
+  let rec aux c = function
+    | [] -> c
+    | _ :: xs -> aux (c + 1) xs in
+  aux 0 l
+
+(* 5. Reverse a list. (easy) *)
+
+let reverse l =
+  let rec aux r = function
+    | [] -> r
+    | x :: xs -> aux (x :: r) xs in
+  aux [] l
+
+(* 6. Find out whether a list is a palindrome. (easy) *)
+
+let is_palindrome l =
+  l = (reverse l)
+
+(* 7. Flatten a nested list structure. (medium) *)
+
+let rec flatten = function
+  | (x :: ys) :: xs -> x :: (flatten ys) :: flatten (xs)
+  | x -> x
